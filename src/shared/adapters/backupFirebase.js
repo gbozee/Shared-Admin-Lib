c3f7cde99d744c33a6ea4274ac54e1f12c589574
Beesama <gbozee@gmail.com>
2018-12-19 20:01:56 +0100

    when token is undefined, use an empty string

--- src/shared/adapters/backupFirebase.js
+++ src/shared/adapters/backupFirebase.js
@@ -85,7 +85,7 @@ function appFireBase(keys) {
     }
   };
 }
-function parseJwt(token) {
+function parseJwt(token="") {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace("-", "+").replace("_", "/");
   return JSON.parse(window.atob(base64));
